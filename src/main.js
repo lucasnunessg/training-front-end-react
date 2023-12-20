7c0fbde0df8bd51b1d76f31bbca06c1ba78ba4d2
LucasNunesDEV <lucasnunespacheco@gmail.com>
2023-12-20 16:54:51 -0300

    finalizando a aplicação de um simples validador utilizando a biblioteca validator

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
-console.log("Hello, World!");
+import validator from 'validator';
+import './style.css';
+
+const campoDeTexto = document.querySelector('#value');
+const button = document.querySelector('#button');
+const seletor = document.querySelector('#option');
+const textoDeSaida = document.querySelector('#answer');
+const isUUID = 4;
+
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const campos = {
+    cpf: validator.isTaxID(campoDeTexto.value, 'pt-BR'),
+    hexColor: validator.isHexColor(campoDeTexto.value),
+    email: validator.isEmail(campoDeTexto.value),
+    uuid: validator.isUUID(campoDeTexto.value, isUUID),
+    url: validator.isURL(campoDeTexto.value),
+  };
+
+  textoDeSaida.innerHTML = `A validação retornou ${campos[seletor.value]}`;
+});
