74b44e874ae1b32d5dcc87f0cae6901a21be0209
LucasNunesDEV <lucasnunespacheco@gmail.com>
2023-12-21 16:16:42 -0300

    praticando promises

---
+++ src/main.js
@@ -0,0 +1,46 @@
+const { error } = require("console");
+
+const generateRandomNumber = () => Math.round(Math.random() * 10);
+
+const resolvedPromise = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const randomNumber = generateRandomNumber();
+      resolve(randomNumber);
+    }, 1000);
+  });
+
+resolvedPromise().then((response) => {
+  console.log(`O número aleatório gerado é: ${response}`);
+})
+
+const rejectPromise = () =>
+  new Promise ((resolve, reject) => {
+    setTimeout(() => {
+      const randomNumber = generateRandomNumber();
+      reject (new Error(`O número ${randomNumber} é inválido!.`))
+    }, 1000);
+  });
+
+  const randomPromise = () => 
+    new Promise ((resolve, reject) => {
+      setTimeout(() => {
+        const randomNumber = generateRandomNumber();
+        if (randomNumber % 2 === 0) {
+          return resolve(randomNumber);
+        
+        } return reject(new Error (`O número ${randomNumber} não pode ser gerado, é ímpar!`))
+    }, 1000);
+  });
+
+  rejectPromise().then((response) => {
+    console.log(`O número aleatório gerado é: ${response}`);
+  }).catch((error) => {
+    console.log(`Rejected promise: ${error.message}`);
+   })
+
+   randomPromise().then(response => {
+    console.log(`O número gerado é: ${response}`);
+   }).catch((error) => {
+    console.log(`Promise rejeitada: ${error.message}`);
+   });
