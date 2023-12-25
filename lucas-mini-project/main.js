c81cef0ff88e6868ddb8c1b181360744f76cd5a1
LucasNunesDEV <lucasnunespacheco@gmail.com>
2023-12-24 21:01:41 -0300

    finalização e estilização

--- lucas-mini-project/main.js
+++ lucas-mini-project/main.js
@@ -1,21 +1,34 @@
-const API_BASE = 'https://akabab.github.io/superhero-api/api';
-
-fetch (`${API_BASE}/all.json`).then((response) => {
-    return response.json().then((data) => console.log(data));
-})
-
+import Swal from 'sweetalert2';
+import './style.css';
 
 const img = document.querySelector('#image');
 const name = document.querySelector('#name');
 const button = document.querySelector('#button');
 
+const BASE_URL = `https://akabab.github.io/superhero-api/api`;
+
+
 const MAX_HEROES = 1000;
 
-const randomId = Math.floor(Math.random() * MAX_HEROES);
+const randomId = () => Math.floor(Math.random() * MAX_HEROES);
 
 button.addEventListener('click', (event) => {
   event.preventDefault();
+
+  const id = randomId();
+
+  fetch(`${BASE_URL}/id/${id}.json`)
+    .then((result) => result.json())
+    .then((data) => {
+
+      img.src = data.images.md;
+      name.innerHTML = data.name;
+    })
+    .catch((error) => Swal.fire({
+
+      title: 'Hero not found',
+      text: error.message,
+      icon: 'error',
+      confirmButtonText: 'Cool',
+    }));
 });
-
-const id = randomId();
-
