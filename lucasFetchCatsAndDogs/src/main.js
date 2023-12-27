f90b97877ffcc3e6b3bc89c8bceaf7234739b787
LucasNunesDEV <lucasnunespacheco@gmail.com>
2023-12-27 15:47:30 -0300

    criando uma aplicação com fetch via-cep

--- lucasFetchCatsAndDogs/src/main.js
+++ lucasFetchCatsAndDogs/src/main.js
@@ -3,29 +3,26 @@ import './style.css';
 const getButtonDog = document.getElementById('randomDog');
 const getButtonCat = document.getElementById('randomCat');
 
-getButtonDog.addEventListener('click', (event) => {
+getButtonDog.addEventListener('click', async (event) => {
     event.preventDefault();
-    fetch('https://dog.ceo/api/breeds/image/random')
-        .then((response) => response.json())
-        .then((data) => {
-            const imgDog = document.getElementById('randomPetImage')
-            imgDog.src = data.message;
-        })
-        .catch((error) => {
-            console.error('Erro ao buscar imagem de cachorro:', error);
+    try {
+        const response = await fetch('https://dog.ceo/api/breeds/image/random');
+        const data = await response.json();
+        const imgDog = document.getElementById('randomPetImage');
+        imgDog.src = data.message;
+    } catch (error) {
+        console.error('Erro ao buscar imagem de cachorro:', `${error.message}`);
+    }
+});
+
+     getButtonCat.addEventListener('click', async (event) => {
+            event.preventDefault();
+            try {
+                const response = await fetch('https://api.thecatapi.com/v1/images/search');
+                const data = await response.json();
+                const imgCat = document.getElementById('randomPetImage');
+                imgCat.src = data[0].url;
+            } catch (error) {
+                console.error('Erro ao buscar imagem de gato:', `${error.message}`);
+            }
         });
-});
-
-getButtonCat.addEventListener('click', (event) => {
-    event.preventDefault();
-    fetch('https://api.thecatapi.com/v1/images/search')
-    .then((response) => response.json()
-    .then((data) => {
-        const imgCat = document.getElementById('randomPetImage')
-        imgCat.src = data[0].url;
-    }))
-    .catch((error) => {
-        console.log('Erro ao buscar imagem de gato:', error);
-    });
-});
-
