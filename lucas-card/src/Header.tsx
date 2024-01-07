e7b0dbdfba4452e715bed4a6ea1aed602444e08b
lucasnunessg <lucasnunespacheco@gmail.com>
2024-01-07 16:18:16 -0300

    criando aplicação de card sobre minhas habilidades

---
+++ lucas-card/src/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header>
+<img src="https://avatars.githubusercontent.com/u/114934451?v=4" alt="Logo"></img>
+<h1> Lucas Nunes </h1>
+<h2>Front End</h2>
+</header>
+  );
+}
+
+export default Header;
