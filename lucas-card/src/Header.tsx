404a705ea844842033245f7b4efa9f2122859391
lucasnunessg <lucasnunespacheco@gmail.com>
2024-01-07 16:43:19 -0300

    criando logica para calcular idade exata conforme a data

--- lucas-card/src/Header.tsx
+++ lucas-card/src/Header.tsx
@@ -1,11 +1,19 @@
+import { calculateAge } from "./utils/age";
+
 function Header() {
+  const birthday = '19/08/1999';
+  const age = calculateAge(birthday);
+
   return (
-    <header>
-<img src="https://avatars.githubusercontent.com/u/114934451?v=4" alt="Logo"></img>
-<h1> Lucas Nunes </h1>
-<h2>Front End</h2>
-</header>
+  <header>
+    <img src="https://avatars.githubusercontent.com/u/114934451?v=4" alt="Logo"></img>
+    <h1> Lucas Nunes, {age}</h1>
+    <h2>Front End</h2>
+    <h3>Natural de São Gabriel-RS, acadêmico de Engenharia de Software, aluno da Trybe</h3>
+  </header>
   );
 }
 
 export default Header;
+
+// <blockquote    cite="https://www.linkedin.com/in/lucas-nunes-750688241/"></blockquote>
