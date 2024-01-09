997fbcbc5ec2ebc10bac2614f7c98372254e5015
LucasNunesDEV <lucasnunespacheco@gmail.com>
2024-01-09 17:14:15 -0300

    implementando numeros aleatórios para treino de props

--- aulatrybe/src/App.tsx
+++ aulatrybe/src/App.tsx
@@ -1,9 +1,13 @@
-import Greeting from "./Greeting"
+import Greeting from "./Greeting";
+import LuckyNumbers from "./LuckyNumbers"; 
 
 function App() {
-
   return (
-<Greeting person={ { firstName: "Chapolin", lastName: "Colorado" } }/>    )
+    <>
+      <Greeting person={{ firstName: "Chapolin", lastName: "Colorado" }} />
+      <LuckyNumbers />
+    </>
+  );
 }
 
-export default App
+export default App;
